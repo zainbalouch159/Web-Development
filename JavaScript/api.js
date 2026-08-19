a102397146b925ee33c954f3e3a13cb22381d603
Zain Balouch <zainbaloch8891@gmail.com>
2026-08-19 10:19:34 +0500

    Improve error handling and refactor loading logic in dog facts API

--- JavaScript/api.js
+++ JavaScript/api.js
@@ -1,7 +1,8 @@
 let i = -1;
 let data;
 let url = "https://dogapi.dog/api/v2/breeds";
-load = document.createElement("div")
+
+let load = document.createElement("div")
 load.id = "load"
 load.innerHTML = `Loading....`
 document.body.append(load)
@@ -12,25 +13,37 @@ btn.innerHTML = `Click to get dog facts`
 document.body.append(btn);
 
 let error1 = document.createElement("div")
-error1.innerHTML = `<h1><center><b>Error</b></center></h1>`
 error1.style.color = "red"
 error1.style.display = "none"
 document.body.append(error1);
-(async () => {
-    btn.style.display = "none"
-    try {
-        let response = await fetch(url)
-        data = await response.json()
-        load.style.display = "none"
-        btn.style.display = "block"
-        console.log(data)
-    }
-    catch (error) {
-        error1.style.display = "block"
-        load.style.display = "none"
 
-    }
-})()
+let prev = document.createElement("button")
+prev.style.margin = "5px"
+prev.style.display = "none"
+prev.innerHTML = `Previous`
+document.body.append(prev)
+
+    (async () => {
+        try {
+            btn.style.display = "none"
+            let response = await fetch(url)
+            if (response.ok) {
+                data = await response.json()
+                load.style.display = "none"
+                btn.style.display = "block"
+                console.log(data)
+            } else {
+                error1.innerText = `Server error: ${response.status}`
+                error1.style.display = "block"
+                load.style.display = "none"
+            }
+        }
+        catch (error) {
+            error1.innerText = `Network error`
+            error1.style.display = "block"
+            load.style.display = "none"
+        }
+    })()
 
 
 btn.addEventListener("click", () => {
@@ -41,20 +54,23 @@ btn.addEventListener("click", () => {
     else if (i > 0) {
         prev.style.display = "block"
     }
-
-
+    if (i <= 0) {
+        prev.style.display = "none"
+    }
     display()
-
 })
+
 function display() {
     let paraold = document.getElementById("des")
     let containerold = document.getElementById("container")
+
     if (containerold) {
         containerold.remove()
     }
     if (paraold) {
         paraold.remove()
     }
+
     let par = document.createElement("p")
     par.id = "des"
     par.innerHTML = `Dog name: <b>${data.data[i].attributes.name}</b> <br> <br>
@@ -76,20 +92,17 @@ function display() {
             let img = document.createElement("img")
             img.src = image.thumb
             container.append(img)
+            
         });
         document.body.append(container)
     }
 }
-let prev = document.createElement("button")
-prev.style.margin = "5px"
-prev.style.display = "none"
-prev.innerHTML = `Previous`
-document.body.append(prev)
+
 prev.addEventListener("click", () => {
     i--;
 
     display()
+    if (i <= 0) {
+        prev.style.display = "none"
+    }
 })
-if (i <= 0) {
-    prev.style.display = "none"
-}
