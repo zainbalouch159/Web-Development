e39d993c6c48e754b2a1c4d004412cd7f09c42ea
Zain Balouch <zainbalouch159@gmail.com>
2026-08-20 17:07:28 +0500

    Refactor first.py by commenting out unused code and adding age validation logic

--- Python/first.py
+++ Python/first.py
@@ -7,6 +7,62 @@
 # print(isalive)
 # name = input("Enter your name ")
 # print("hello " + name) #Concatenation
-age = input("Enter your age ")
-age = age+1
-print(type(age))
+
+# Type casting & Type conversion
+
+# age = input("Enter your age ")
+# age1=int(age)
+# age1 = age1+1
+# print(age1)
+# print(float(age1)) # Type casting explicit
+# print(bool(age1))
+# print(str(age1))
+# print(type(age1)) 
+# print(2+2.5) # Type conversion implicit
+
+# String Method
+
+name = "Zain Khan" # String are immutable
+# print(name.upper())
+# print(name)
+
+# # Find 
+
+# print(name.find("K"))
+
+# Replace
+
+# print(name.replace("Khan","Balouch"))
+
+# Check for presence
+
+# print("K" in name) # in is keyword 
+
+# Artihmatic operators
+
+# print(4+3)
+# print(4-3)
+# print(4*3)
+# print(4/3)
+# print(4//3)
+# print(4%3) # Modulo-> Reminder
+# print(4**3)
+
+# assigment operator
+
+# cgpa = 9.2
+# cgpa = cgpa +2
+# cgpa += 3
+# print(cgpa)
+
+# Operator Precedence
+#   *,/ > +,-
+
+# print(not(1>2))
+
+age = 18
+
+if age >18 :
+    print("Allowed")
+else :
+    print("Not Allowed")
