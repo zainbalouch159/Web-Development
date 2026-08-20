6fd8703b8cb74ee04de31d77a7c72d833515ef29
Zain Balouch <zainbaloch8891@gmail.com>
2026-08-20 11:59:33 +0500

    Add first.py with age input functionality and type printing

---
+++ Python/first.py
@@ -0,0 +1,12 @@
+# print("Zain","Balouch")
+# name = "Zain"
+# age = 22
+# print(name,age)
+# print(type(age))
+# isalive = False
+# print(isalive)
+# name = input("Enter your name ")
+# print("hello " + name) #Concatenation
+age = input("Enter your age ")
+age = age+1
+print(type(age))
