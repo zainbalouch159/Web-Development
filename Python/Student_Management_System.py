906fbecb5486260b96168499d7f3e739e1ac9776
Zain Balouch <zainbalouch159@gmail.com>
2026-08-22 17:27:44 +0500

    Enhance student management system; improve input prompts, display logic, and add edit functionality

--- Python/Student_Management_System.py
+++ Python/Student_Management_System.py
@@ -1,7 +1,7 @@
 class Student:
     
     def add_student(self):
-        self.name = input("Enter student name: ")
+        self.name = input("\nEnter student name: ")
         self.rollno = int(input("Enter roll no: "))
         total_subject = int(input("How many subject in last semester: "))
         
@@ -15,28 +15,51 @@ class Student:
         self.cgpa= float(input("What your cgpa: "))
         
     def show(self):
+
         if isinstance(self.name,(str)):
-            print("Your name is",self.name)
+            print("\nYour name is",self.name)
         else:
             print("Invalid data")
+
         if isinstance(self.rollno,(int)) and self.rollno>0:
             print("Roll no:",self.rollno)
         else:
             print("Invalid data")
+
         if isinstance(self.marks, (dict)):
             for key, value in self.marks.items():
                 print(key,"=",value)
-        if isinstance(self.marks,(int,float)) and self.marks>0:
-            print("Marks:",self.marks)
+
         if isinstance(self.cgpa,(float,int)) and self.cgpa> 0:
             print("CGPA:",self.cgpa)
         else:
             print("Invalid data")
+            
+    def edit(self):
+        edit_name = input("Which student data you want to edit? ")
+        choice = int(input("What you want to edit?\n 1: Name\n 2: roll No\n 3: Marks \n 4: CGPA \n 5: Back"))
+        for student in new_student:
+            if student.name == self.edit_name:
+                match choice:
+                    case 1:
+                        new_name = input("Enter new name ")
+                        student.name=new_name
+                    case 2:
+                        new_rollno = int(input("Enter new roll no "))
+                        student.rollno=new_rollno
+                    case 3:
+                        for key, value in student.marks.items():
+                            print(key,"=",value)
+                        key_name = input("Enter name that you want to edit ")
+                        key_reference = input("You want to edit data name or acutal data\n Press 1 for data name\n Press 2 for actual data")
+                        match key_reference:
+                            case 1:
+                                pass
 
 total_student = []
 
 while True:
-    print("===== Student Management =====")
+    print("\n===== Student Management =====\n")
     print("1. Add Student")
     print("2. Show Student")
     print("3. Edit")
@@ -52,8 +75,9 @@ while True:
             if total_student:
                 for student in total_student:
                     student.show()
+        
+        case 3:
+            pass
 
     if choice == 0:
         break
-
-print(total_student)
