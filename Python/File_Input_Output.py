e5a414b010acaa5c613a94f5fc9480b44b7fbd98
Zain Balouch <zainbaloch8891@gmail.com>
2026-08-23 10:15:46 +0500

    Add File_Input_Output.py; include file handling examples and remove temporary file

---
+++ Python/File_Input_Output.py
@@ -0,0 +1,16 @@
+# a = open("Demo.txt","a+")
+data=" Balouch "
+# a.write(data)
+# a.seek(0)
+# data1=a.readline()
+# a.close()
+# print(data1)
+
+# with open("Demo.txt","a+") as f:
+#     f.write(data)
+#     f.seek(0)
+#     print(f.read())
+    
+import os
+
+os.remove("tempCodeRunnerFile.py")
