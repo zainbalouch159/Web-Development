bbf3cf797de1704223b2fafbef7c39e088f46145
Zain Balouch <zainbaloch8891@gmail.com>
2026-08-24 08:11:24 +0500

    Enhance student management system; improve input validation and error handling in add_student and marks_edit methods

--- Python/Student_Management_System.py
+++ Python/Student_Management_System.py
@@ -1,86 +1,181 @@
+
+total_student = []
 class Student:
     
-    def add_student(self):
-        self.name = input("\nEnter student name: ")
-        self.rollno = int(input("Enter roll no: "))
-        total_subject = int(input("How many subject in last semester: "))
+    def add_student(self): # -->  Student add fun
+        while True:
+            self.name = input("\nEnter student name: ")
+            if self.name.isalpha():
+                break
+            else:
+                print("\nEnter valid name")
+            
+        while True:
+            try:
+                self.rollno = int(input("\nEnter roll no: "))
+               
+                if self.rollno>0 and self.rollno<100:
+                    break
+                else:
+                    print("\nInvalid roll no")
+            except ValueError:
+                            print("\nInvalid roll no")
+            
+        while True:
+            try:    
+                total_subject = int(input("\nHow many subject in last semester: "))
+                if total_subject>10 or total_subject<1:
+                    print("\nInvalid input")
+                else:
+                    break
+            except ValueError:
+                print("\nInvalid input")
         
         self.marks = {}
-        for i in range(total_subject):
-            print("\t<<(",i+1,")>>")
-            subject_name=input("Enter subject name of last semester: ")
-            subject_marks= int(input(f"How many marks you got in {subject_name}: "))
-            self.marks[subject_name]=subject_marks
-            
-        self.cgpa= float(input("What your cgpa: "))
         
-    def show(self):
+        for i in range(total_subject):
+            print("\n\t<<(",i+1,")>>")
+            while True:
+                subject_name=input("\nEnter subject name of last semester: ")
+                if subject_name.isalpha():
+                    break
+                else:
+                    print("\nPlease enter text")
+            while True:
+                try:
+                    subject_marks= int(input(f"\nHow many marks you got in {subject_name}: "))
+                    if subject_marks>100 or subject_marks<0:
+                        print("Invalid input")
+                    else:
+                        self.marks[subject_name]=subject_marks
+                        break
+                except ValueError:
+                    print("\nInvalid input")
+        while True:
+            try:
+                self.cgpa= float(input("\nWhat your cgpa: "))
+                if self.cgpa>4 or self.cgpa<0:
+                    print("\nInvalid input")
+                else:
+                    break
+            except ValueError:
+                print("\nInvalid input")
+                
+    def marks_edit(self): # ---> Marks edit fun
+        
+     while True: # 4 
+            print("\n")
+            
+            for key, value in self.marks.items():
+                print(key,"=",value)
+                
+            while True: # 5
+                key_name = input("\nEnter name that you want to edit ")
+                if key_name.isalpha():
+                    break # Terminate 5
+                else:
+                    print("\nPlease enter text")
+        
+            if key_name in self.marks:
+                value = self.marks[key_name]
+                while True: # 7 
+                    try:
+                        key_reference = int(input("\nYou want to edit data name or acutal data\nPress 1 for data   name\nPress 2  for actual data\n"))
+                        break # Terminate 7
+                    except ValueError:
+                        print("\nInput invalid")
+        
+                match key_reference:
+                    case 1:
+                        while True: # 8
+                            new_key= input(f"\nEnter name that you want to replace with {key_name} ")
+                            if new_key.isalpha():
+                                self.marks[new_key]=value
+                                del self.marks[key_name]
+                                break # Terminate 8
+                            else:
+                                print("\nPlease enter text")
+        
+                        break # Terminate 4
+        
+                    case 2:
+                        while True:
+                            try:
+                                new_value= int(input(f"\nEnter data that you want to replace wit {value}"))
+                                self.marks[key_name]=new_value
+                                break
+                            except ValueError:
+                                print("Invalid input")     
+            else:
+                print("\nNo data with ",key_name," such name exists")
+            
+    def edit(self): # --> Student info edit fun
+        while True: # -> 1
+            try:
+                choice = int(input("\nWhat you want to edit?\n 1: Name\n 2: roll No\n 3: Marks \n 4: CGPA \n 5: Back\n"))
+                break # -> Terminate 1
+            except ValueError:
+                print("\nInvalid input")
+        match choice:
+            case 1:
+                while True: # -> 2
+                    new_name = input("\nEnter new name ")
+                    if new_name.isalpha():
+                        self.name=new_name
+                        break # -> Terminate 2
+                    else:
+                        print("\nPlease enter text")
+                        
+            case 2:
+                while True: # 3
+                    try:
+                        new_rollno = int(input("\nEnter new roll no "))
+                        if new_rollno>0 and new_rollno<100:
+                            self.rollno=new_rollno
+                            break # -> Terminate 3
+                        else:
+                            print("\nInvalid roll no")
+                    except ValueError:
+                        print("\nInvalid input")
+                
+            case 3:
+                self.marks_edit()
+                               
+            case 4:
+                while True:
+                    try:
+                        new_cgpa = float(input(f"\nEnter new CGPA that you want to replace with {self.cgpa} "))
+                        if new_cgpa>4 or new_cgpa<0:
+                            print("Invalid input")
+                        else:         
+                            self.cgpa= new_cgpa
+                            break
+                    except ValueError:
+                        print("\nInvalid input")
+                        
+    def show(self): # --> Student info show fun
 
-        if isinstance(self.name,(str)):
             print("\nYour name is",self.name)
-        else:
-            print("Invalid data")
 
-        if isinstance(self.rollno,(int)) and self.rollno>0:
-            print("Roll no:",self.rollno)
-        else:
-            print("Invalid data")
+            print("\nRoll no:",self.rollno)
 
-        if isinstance(self.marks, (dict)):
             print("\n===== Marks =====\n")
             for key, value in self.marks.items():
                 print(key,"=",value)
 
-        if isinstance(self.cgpa,(float,int)) and self.cgpa> 0:
             print("\nCGPA:",self.cgpa)
-        else:
-            print("Invalid data")
             
-    def edit(self):
-        choice = int(input("What you want to edit?\n 1: Name\n 2: roll No\n 3: Marks \n 4: CGPA \n 5: Back\n"))
-        
-        match choice:
-            case 1:
-                new_name = input("Enter new name ")
-                self.name=new_name
-            case 2:
-                new_rollno = int(input("Enter new roll no "))
-                self.rollno=new_rollno
-            case 3:
-                for key, value in self.marks.items():
-                    print(key,"=",value)
-                key_name = input("\nEnter name that you want to edit ")
-                for key, value in self.marks.items():
-                    if key == key_name:
-                        key_reference = int(input("You want to edit data name or acutal data\n Press 1 for data name\nPress 2  for actual data\n"))
-                        match key_reference:
-                            case 1:
-                                new_key= input(f"Enter name that you want to replace with {key} ")
-                                self.marks[new_key]=value
-                                del self.marks[key]
-                            case 2:
-                                if isinstance(value,(int)):
-                                    new_value= int(input(f"Enter data that you want to replace wit {value}"))
-                                    self.marks[key]=new_value
-                                if isinstance(value,(float)):
-                                    new_value= float(input(f"Enter data that you want to replace wit {value}"))
-                                    self.marks[key]=new_value
-                                if isinstance(value,(str)):
-                                    new_value= input(f"Enter data that you want to replace wit {value}")
-                                    self.marks[key]=new_value
-                        break       
-            case 4:
-                new_cgpa = float(input(f"Enter new CGPA that you want to replace with {self.cgpa} "))         
-                self.cgpa= new_cgpa
-            
-total_student = []
-
-while True:
-    print("\n===== Student Management =====\n")
-    print("1. Add Student")
-    print("2. Show Student")
-    print("3. Edit")
-    choice=int(input("0. Exit\n"))
+while True: # --> Main Menu 
+    while True:
+        try:
+            print("\n===== Student Management =====\n")
+            print("1. Add Student")
+            print("2. Show Student")
+            print("3. Edit")
+            choice=int(input("0. Exit\n"))
+            break
+        except ValueError:
+            print("\nInvalid input")
     
     match choice:
         case 1:
@@ -93,23 +188,30 @@ while True:
                 for student in total_student:
                     student.show()
             else:
-                print("No student record is saved")
+                print("\nNo student record is saved")
         
         case 3:
             if len(total_student)>1:
                 for student in total_student:
                     print(student.name)
-                edit_name = input("Which student data you want to edit? ")
+                while True:
+                    edit_name = input("\nWhich student data you want to edit? ")
+                    if edit_name.isalpha():
+                        break
+                    else:
+                        print("\nPlease enter text")
                 for student in total_student:
                     if student.name == edit_name:
                         student.edit()
                         break
+                else:
+                    print("\nNo such name ", edit_name," exists")
             elif len(total_student)<1 :
-                 print("No student record is saved")
-            else:
+                 print("\nNo student record is saved")
+            elif len(total_student)==1:
                 total_student[0].edit()
             
-    if choice == 0:
-        break
+        case 0:
+            break
     
 print("\nExit Successfully")
