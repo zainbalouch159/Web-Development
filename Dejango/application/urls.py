c4d3c489e263b03771980d284d59ecb0a096bb11
Zain Balouch <zainbaloch8891@gmail.com>
2026-08-27 09:34:37 +0500

    Add form for note creation and update navigation links
    - Implement note creation form in index.html with CSRF token and input fields for title and description.
    - Update navbar.html to use Django URL template tags for navigation links.
    - Include Font Awesome for icons in base.html.
    - Add detail_save view and corresponding URL pattern for handling form submissions.

--- Dejango/application/urls.py
+++ Dejango/application/urls.py
@@ -1,7 +1,7 @@
-from .views import indexview,aboutview
+from .views import indexview,aboutview,detail_save
 from django.urls import path
 urlpatterns=[
  path("",indexview,name="index"),
-    path("about",aboutview,name="about")
-    
+    path("about",aboutview,name="about"),
+    path("detail-save",detail_save,name="detail_save")
 ]
--- Dejango/application/views.py
+++ Dejango/application/views.py
@@ -3,8 +3,14 @@ from django.shortcuts import render
 # Create your views here.
 
 from django.http import HttpResponse
+
 def indexview(req,):
     return render(req,"index.html")
+
 def aboutview(req):
-    return render(render,"about.html")
+    return render(req,"about.html")
+
+def detail_save(req):
+    print(req.POST)
+    return HttpResponse("Detail saved")
 
