89e79a471c27b1642ea6b6eb93c8b29586e062d9
Zain Balouch <zainbaloch8891@gmail.com>
2026-08-30 10:22:20 +0500

    Update ALLOWED_HOSTS in settings.py to include production and local hosts

--- Dejango/new_project/settings.py
+++ Dejango/new_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-rl!$=dco0v@unh$tv$o%)wbzsqzoq&ud%pwg*#n1dnfoo&+iw6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ZainBablouch.pythonanywhere.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
