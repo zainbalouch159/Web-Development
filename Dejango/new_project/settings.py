261e22d96ad80088a7127fc999ef338f31393b48
Zain Balouch <zainbaloch8891@gmail.com>
2026-08-30 10:31:47 +0500

    Add STATIC_ROOT setting to configure static files directory

--- Dejango/new_project/settings.py
+++ Dejango/new_project/settings.py
@@ -114,9 +114,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.1/howto/static-files/
-
+import os
 STATIC_URL = 'static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Email
 # https://docs.djangoproject.com/en/6.1/topics/email/#topic-email-configuration
