d01e7946a47872a594cbd1cf6a1d4c615cdd16ff
Zain Balouch <zainbaloch8891@gmail.com>
2026-08-31 08:05:27 +0500

    Add NoteAdmin class to customize admin interface for Note model

--- Django/application/admin.py
+++ Django/application/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
 from .models import Note
 # Register your models here.
+class NoteAdmin(admin.ModelAdmin):
+    list_display = ('Title', 'isPublish', 'Created_AT')
 admin.site.register(Note)
