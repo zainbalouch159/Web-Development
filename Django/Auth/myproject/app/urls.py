5d11a5d5fe7b4682387d48f78d89d56d49de6a84
Zain Balouch <zainbalouch159@gmail.com>
2026-09-01 16:57:30 +0500

    Implement user registration feature with form validation and dashboard redirection

--- Django/Auth/myproject/app/urls.py
+++ Django/Auth/myproject/app/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path("",views.app,name="app")
+    path("",views.register,name="register")
 ]
--- Django/Auth/myproject/app/views.py
+++ Django/Auth/myproject/app/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render,redirect
-
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm
 # Create your views here.
 
-def app(req):
-    return render(req,"register.html")
+def register(req):
+    if req.method=="POST":
+        form = UserCreationForm(req.POST)
+        if form.is_valid():
+            form.save()
+            return render(req,"dashboard.html")
+        else:
+            return render("register", {"form":form})
