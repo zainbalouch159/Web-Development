14fba3d0659e96033ead306ec733a41f4cc409e7
Zain Balouch <zainbaloch8891@gmail.com>
2026-09-02 10:56:01 +0500

    Add Tailwind CSS dependencies to package.json

--- Django/Auth/myproject/app/urls.py
+++ Django/Auth/myproject/app/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path("",views.register,name="register")
+    path("",views.register,name="register"),
+    path("/login",views.Login,name="login"),
+    path("/dashboard", views.dashboard, name="dashboard")
 ]
--- Django/Auth/myproject/app/views.py
+++ Django/Auth/myproject/app/views.py
@@ -8,6 +8,26 @@ def register(req):
         form = UserCreationForm(req.POST)
         if form.is_valid():
             form.save()
-            return render(req,"dashboard.html")
+            return redirect("dashboard")
         else:
-            return render("register", {"form":form})
+            return render(req,"register.html", {"form":form})
+    else:
+        return render(req,"register.html")
+    
+def Login(req):
+    if req.method == 'POST':
+        username = req.POST.get("username")
+        password = req.POST.get("password")
+        if not username and not password:
+            return redirect("login")
+        else:
+            user = authenticate(request=req,username=username,password=password)
+            if user is not None:
+                login(req,user)
+                return redirect("dashboard")
+            else:
+                error= "Invalid password or username "
+                return render(req,"login.html",{"error":error})
+    return render(req,"login.html")
+def dashboard(req):
+    return render(req,"dashboard.html")
