e5fb6dbdb0e7f23ffa6020460b114907969d4f0c
Zain Balouch <zainbaloch8891@gmail.com>
2026-09-07 09:33:12 +0500

    refactor: Enhance navbar functionality and styles for improved user experience

--- Project/FAZS Ecommerce/myproject/static/js/navbar.js
+++ Project/FAZS Ecommerce/myproject/static/js/navbar.js
@@ -1,13 +1,20 @@
-searchicon = document.getElementById("search")
+searchicon = document.getElementById("search");
+
 searchicon.addEventListener("click", () => {
+    search_input = document.getElementById("search_input");
 
-      search_input.classList.toggle("translate-x-full");
-    search_input.classList.toggle("translate-x-0");
+        if (search_input.value.trim()) {
+          searchicon.type="submit"
 
-    search_input.classList.toggle("opacity-0");
-    search_input.classList.toggle("opacity-100");
+        }else{
+          searchicon.type="button"
 
-    search_input.classList.toggle("pointer-events-none");
+        search_input.classList.toggle("translate-x-full");
+        search_input.classList.toggle("translate-x-0");
 
+        search_input.classList.toggle("opacity-0");
+        search_input.classList.toggle("opacity-100");
 
+        search_input.classList.toggle("pointer-events-none");
+        }
 });
