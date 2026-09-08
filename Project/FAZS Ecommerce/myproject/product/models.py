cae248fcd556de7b8e8ac55418b3d34b19832e94
Zain Balouch <zainbaloch8891@gmail.com>
2026-09-08 20:04:35 +0500

    feat: Add string representation methods for Category and Collection models; fix Product model's ForeignKey reference

--- Project/FAZS Ecommerce/myproject/product/models.py
+++ Project/FAZS Ecommerce/myproject/product/models.py
@@ -5,10 +5,25 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(max_length=100)
     
+    def __str__(self):
+        return self.name
+    
+    
+class Collection(models.Model):
+    title = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.title
+
+    
 class Product(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
     price = models.IntegerField()
     stock = models.IntegerField()
     image = models.ImageField()
-    Category = models.ForeignKey()
+    discount = models.IntegerField(default=0)
+    category = models.ForeignKey(Category,on_delete=models.SET_DEFAULT,default=1)
+    collection =models.ManyToManyField(Collections)
+    def __str__(self):
+        return self.name
