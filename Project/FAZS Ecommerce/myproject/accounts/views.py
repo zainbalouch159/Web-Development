95f90829656c7328aae0b874d209c90021a4bb12
Zain Balouch <zainbaloch8891@gmail.com>
2026-09-08 10:09:05 +0500

    refactor: Update login and registration functionality; create registration template and fix login view

--- Project/FAZS Ecommerce/myproject/accounts/views.py
+++ Project/FAZS Ecommerce/myproject/accounts/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from django.contrib.auth import authenticate,login,logout
+from django.contrib.auth.forms import UserCreationForm
+
 # Create your views here.
 
-def login(req):
+def login_view(req):
     if req.method=='POST':
         username = req.POST.get('Username')
         password = req.POST.get('password')
@@ -15,3 +17,15 @@ def login(req):
             return render(req,'login.html',{'error':'Username or password is not correct'})
     else:
         return render(req,'login.html')
+    
+def register(req):
+    if req.method=='POST':
+        form = UserCreationForm(req.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+        else:
+            return render(req,'register.html',{'form':form})
+    else:
+            return render(req,'register.html')
+        
