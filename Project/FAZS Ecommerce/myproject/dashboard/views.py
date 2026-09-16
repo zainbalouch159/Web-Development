331572b1ae9998788fbaade5b740692440385a66
Zain Balouch <zainbalouch159@gmail.com>
2026-09-16 07:10:44 +0500

    feat: Enhance product management; update product display with dynamic data and improve add_product functionality

--- Project/FAZS Ecommerce/myproject/dashboard/views.py
+++ Project/FAZS Ecommerce/myproject/dashboard/views.py
@@ -9,17 +9,6 @@ def dashboard(req):
     product =Product.objects.all()
     collection =Collection.objects.all()
     return render(req,'dashboard.html',{'Products':product,'Categories':category,'Collections':collection})
-
-def add_product(req):
-    Name = req.POST.get('Name')
-    Description = req.POST.get('Description')
-    Price = req.POST.get('Price')
-    Stock = req.POST.get('Stock')
-    image = req.POST.get('image')
-    Discount = req.POST.get('Discount')
-    Info = req.POST.get('Info') 
-    Category = req.POST.get('Category')
-    Collections = req.POST.get('Collections')
     
 def add_category(req):
     new_category =  req.POST.get('category-name')
@@ -47,3 +36,36 @@ def add_Collections(req):
             'title':collection.title
         })
        
+def add_product(req):
+    Name = req.POST.get('name')
+    Description = req.POST.get('description')
+    Price = req.POST.get('price')
+    Stock = req.POST.get('stock')
+    image = req.FILES.get('image')
+    Discount = req.POST.get('discount')
+    Info = req.POST.get('info') 
+    Category_id = req.POST.get('category')
+    Collections_id = req.POST.get('collections')
+    Category = Category.objects.get(id=Category_id)
+    Collections = Collection.objects.get(id=Collections_id)
+
+    product =Product(
+        name = Name,
+        description = Description,
+        price = Price,
+        stock = Stock,
+        image = image,
+        discount = Discount,
+        info = Info,
+        category = Category,
+    )
+    product.save()
+    product.collections.add(Collections)
+    return JsonResponse({
+        'id': product.id,
+        'name' : product.name,
+        'Price' : product.price,
+        'sales' : product.sales,
+        'image' : product.image,
+       
+    })
